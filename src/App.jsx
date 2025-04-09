29403ea2684948d8d56b0a461c2e50b3a3615a59
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-04-09 21:38:53 +0545

    React Query Setup with its devtools

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,40 @@ import Settings from './pages/Settings';
 import Users from './pages/Users';
 import Login from './pages/Login';
 import AppLayout from './components/AppLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const StyledApp = styled.div``;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // 1 min
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
-      <StyledApp>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="account" element={<Account />} />
-              <Route path="rooms" element={<Rooms />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="users" element={<Users />} />
-              {/* Redirect to dashboard if no route matches */}
-              <Route index element={<Navigate replace to="dashboard" />} />
-            </Route>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="account" element={<Account />} />
+            <Route path="rooms" element={<Rooms />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+            {/* Redirect to dashboard if no route matches */}
+            <Route index element={<Navigate replace to="dashboard" />} />
+          </Route>
 
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </StyledApp>
-    </>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
