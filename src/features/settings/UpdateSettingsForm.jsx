d574ce5cc78e53a1d40af4acb85fa4fdabfaa3ad
Sujit Kumar Libi <sujit_libi@hotmail.com>
2025-04-12 22:17:15 +0545

    fetching and updating application settings feature implemented

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,71 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from '../../components/Form';
+import FormRow from '../../components/FormRow';
+import Input from '../../components/Input';
+import Spinner from '../../components/Spinner';
+import { useSettings } from './hooks/useSettings';
+import { useUpdateSetting } from './hooks/useUpdateSetting';
 
 function UpdateSettingsForm() {
+  const {
+    isLoading,
+    settings: {
+      min_booking_length,
+      max_booking_length,
+      max_guest_per_length,
+      breakfast_price,
+    } = {},
+  } = useSettings();
+  const { isUpdating, updateSetting } = useUpdateSetting();
+
+  if (isLoading) return <Spinner />;
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+
+    if (!value) return;
+    updateSetting({ [field]: value });
+  }
+
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={min_booking_length}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'min_booking_length')}
+        />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+
+      <FormRow label="Maximum nights/booking">
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={max_booking_length}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'max_booking_length')}
+        />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={max_guest_per_length}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'max_guest_per_length')}
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfast_price}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, 'breakfast_price')}
+        />
       </FormRow>
     </Form>
   );
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,14 @@
+import UpdateSettingsForm from '../features/settings/UpdateSettingsForm';
 import Heading from '../components/Heading';
+import Row from '../components/Row';
 
 function Settings() {
-  return <Heading as="h1">Update hotel settings</Heading>;
+  return (
+    <Row>
+      <Heading as="h1">Update hotel settings</Heading>
+      <UpdateSettingsForm />
+    </Row>
+  );
 }
 
 export default Settings;
